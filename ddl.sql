8f78ac56965b509ed96ed6787d763e69a4b41263
LetsMelon <domi.m@outlook.com>
2023-05-21 15:24:21 +0200

    add ddl for spar tables

--- ddl.sql
+++ ddl.sql
@@ -3,7 +3,7 @@ create table if not exists bc_billa_category (
     bc_id uuid default gen_random_uuid() primary key,
     bc_text character varying(256) not null
 );
-drop table if exists bcw_billa_crawl;
+cs_crawl_session drop table if exists bcw_billa_crawl;
 create table if not exists bcw_billa_crawl (
     bcw_id uuid default gen_random_uuid() primary key,
     bcw_created timestamp default current_timestamp
@@ -50,3 +50,46 @@ ALTER TABLE bp_billa_price
 ADD CONSTRAINT bp_billa_price_fk FOREIGN KEY (bp_bpo_product) REFERENCES bpo_billa_product(bpo_id);
 ALTER TABLE bp_billa_price
 ADD CONSTRAINT bp_billa_raw_fk FOREIGN KEY (bp_br_raw) REFERENCES br_billa_raw(br_id);
+drop table if exists sc_spar_category;
+create table if not exists sc_spar_category (
+    sc_id uuid default gen_random_uuid() primary key,
+    sc_text character varying(256) not null
+);
+drop table if exists sr_spar_raw;
+create table if not exists sr_spar_raw (
+    sr_id uuid default gen_random_uuid() primary key,
+    sr_raw text,
+    sr_created timestamp default current_timestamp,
+    sr_url character varying(256) not null,
+    sr_err text default null,
+    sr_cs_crawl_session uuid not null
+);
+ALTER TABLE sr_spar_raw
+ADD CONSTRAINT sr_spar_raw_crawler_fk FOREIGN KEY (sr_cs_crawl_session) REFERENCES bcw_billa_crawl(bcw_id);
+drop table if exists sp_spar_product;
+create table if not exists sp_spar_product (
+    sp_id uuid default gen_random_uuid() primary key,
+    sp_created timestamp default current_timestamp,
+    sp_spar_id character varying(13) not null,
+    sp_description text,
+    sp_online_shop_url character varying(256) not null,
+    sp_name character varying(256) not null,
+    sp_brand character varying(256),
+    sp_sc_category uuid not null
+);
+ALTER TABLE sp_spar_product
+ADD CONSTRAINT sp_spar_product_category_fk FOREIGN KEY (sp_sc_category) REFERENCES sc_spar_category(sc_id);
+drop table if exists spr_spar_price;
+create table if not exists spr_spar_price (
+    spr_id uuid default gen_random_uuid() primary key,
+    spr_p_created timestamp default current_timestamp,
+    spr_price float,
+    spr_sales_unit character varying(256),
+    spr_price_unit character varying(256),
+    spr_sp_product uuid not null,
+    spr_sr_raw uuid not null
+);
+ALTER TABLE spr_spar_price
+ADD CONSTRAINT spr_spar_price_product_fk FOREIGN KEY (spr_sp_product) REFERENCES sp_spar_product(sp_id);
+ALTER TABLE spr_spar_price
+ADD CONSTRAINT spr_spar_price_raw_fk FOREIGN KEY (spr_sr_raw) REFERENCES sr_spar_raw(sr_id);
